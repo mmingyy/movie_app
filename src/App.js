22de741ee65322a9fb3257386b6402e730a9088d
min <studyg00gle@gmail.com>
2020-07-16 15:47:23 +0900

    see img, titile, description on webpage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getmovies = async () => {
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });//{movies(state꺼):movies(axios꺼)}
+  };
+  async componentDidMount() { //render시 호출됨
+    this.getmovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;//state로부터 isLoading,movies가져옴
+    return (<section class="container">
+      {isLoading ? (
+        <div class="loader">
+          <span class="loader__text">Loading..</span>
+        </div>
+      ) : (
+          <div class="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}//list의 child는 unique한 key prop가져야
+                id={movie.id} year={movie.year}
+                title={movie.title} summary={movie.summary}
+                poster={movie.medium_cover_image} />
+
+            ))}</div>
+      )}
+          </section>
+    );
+  }
 }
-
 export default App;
